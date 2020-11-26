bd5f8b7499ef8e46668f4f99beba966bb55a1f7f
Tomek <nawrottomek@gmail.com>
2020-11-26 20:12:52 +0100

    Zaliczenie zad2

--- src/test/java/ZAD1/Zadanie1zal.java
+++ src/test/java/ZAD1/Zadanie1zal.java
@@ -3,14 +3,12 @@ package ZAD1;
 
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import java.util.List;
+
 import java.util.concurrent.TimeUnit;
 
 public class Zadanie1zal {
